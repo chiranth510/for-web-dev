affe9279c9f84193864cfe12f0ecbe975b1b8b45
chiranth510 <155095392+chiranth510@users.noreply.github.com>
2025-03-29 12:58:33 +0530

    Update xx.py

--- xx.py
+++ xx.py
@@ -1,12 +1,13 @@
-def fibonacci(nums):
-    n = len(nums)
-    dp = [0] * n
+def fibonacci(n):
     if n == 0:
         return 0
     if n == 1:
         return 1
-    dp[0] = 0
+
+    dp = [0] * (n + 1)
     dp[1] = 1
-    for i in range(2, n):
-        dp[i] = dp[i-1] + dp[i - 2]
-    return dp[-1]
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
